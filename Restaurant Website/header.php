d247f7ba235344c44d32e8e4aff1a949bd79b079
17-Vishal <katiyarvishal0011@gmail.com>
2020-12-03 16:56:48 +0530

    add php files

---
+++ Restaurant Website/header.php
@@ -0,0 +1,24 @@
+<?php
+    $companyName="Vishal's Fine Dining";
+include('arrays.php')
+        ?>
+
+<html lang="fr">
+<head>
+    <title>
+      VERSHA'S
+    </title>
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body id="final-example">
+<div class="wrapper">
+    <div id="banner">
+        <a href="/" title="Return to Home">
+            <img src="banner3.jpg" alt="Vishal's Fine Dining" >
+        </a>
+    </div>
+
+    <div id="nav">
+          <?php include('nav.php'); ?>
+    </div>
+    <div class="content">
